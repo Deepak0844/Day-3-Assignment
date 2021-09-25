3cca72e9dd963d2237ce6f0a5d4f345a8f01ea7b
Deepak0844 <91111526+Deepak0844@users.noreply.github.com>
2021-09-25 19:00:08 +0530

    Day -3 Assignment(JSON )

---
+++ 2.JSON resume.js
@@ -0,0 +1,50 @@
+{
+    "basics"  [{
+      "name" : "Deepak Ram",
+      "email": "deepakram0105@gmail.com",
+      "phone": "8608641557",
+      "summary": "Aspiring fresher looking for a role of programmer",
+      "location": {
+        "address": "12th street viduthalai nagar s.kolathur",
+        "postalCode": "600117",
+        "city": "chennai",
+        "countryCode": "IN",
+        "region": "India"
+      }
+    }]
+      "profiles" [{
+        "network": "Linkedin",
+        "username": "Deepak Ram",
+        "url": "https://www.linkedin.com/in/deepak-ram-355972212?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BfbI1Qsd7Rmenf5REZHGueQ%3D%3D"
+      }]
+  
+    "education" [{
+      "institution": "Vels University",
+      "url": "http://www.velsuniv.ac.in/",
+      "department": "Mechanical Engineering",
+      "degree": "Bachelor",
+    
+    }],
+
+      "skills" [{
+      "name": "Web Development",
+      "level": "Beginner",
+      "keywords": [
+        "HTML",
+        "CSS",
+        "JavaScript"
+      ]
+    }],
+    "languages known" [{
+      "language": "English , Tamil"
+     }]
+    "interests" [{
+      "name": "playing cricket ,mobile games,listening to music"
+    
+    }],
+   
+    "projects" [{
+      "name": " RECYCLE OF WASTE ENGINE OIL ",
+  
+    }]
+  }
